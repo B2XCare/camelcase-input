ec76767b6e81d2fbb2cc3fc5ee1a092a4a557410
CPatil <chetan.patil@b2x.com>
2018-06-11 14:36:28 +0530

    nested object conversion issue resolved

--- test/convertCase.js
+++ test/convertCase.js
@@ -32,11 +32,31 @@ describe('CONVERT INPUT TO lowerCamelcase:', () => {
   })
 
   it('Should convert array of objects (deep = true) input to lowerCamelcase', (done) => {
-    let output = camelcase([{'FOo-bar': [{'abc-df__r': true}, {'tghd_dfdf--ee': true}]}, {'bar-foo': { 'Test-te': {'opt-tdt': 'dfdfdf'} }}], { deep: true })
+    let output = camelcase([{'Foo-bar': [{'abc-df__r': true}, {'tghd_dfdf--ee': true}]}, {'bar-foo': { 'Test-te': {'opt-tdt': 'dfdfdf'} }}], { deep: true })
     expect(output).to.be.an('array')
     expect(output[0]).to.have.property('fooBar')
     expect(output[0]['fooBar']).to.be.an('array')
     expect(output[0]['fooBar'][0]).to.have.property('abcDfR')
+    done()
+  })
+
+  it('Should convert nested objects (deep = true) input to lowerCamelcase', (done) => {
+    let output = camelcase({
+      attributes: {
+        type: 'Account',
+        url: '/0012500000qP7qcAAC'
+      },
+      Id: 'adsff2324',
+      IsDeleted: false,
+      MasterRecordId: null,
+      Name: 'With Contact',
+      Type: null
+    }, { deep: true })
+
+    expect(output).to.have.property('attributes')
+    expect(output['attributes']).to.be.an('object')
+    expect(output['attributes']).to.have.property('type')
+    expect(output).to.have.property('isDeleted')
     done()
   })
 })
