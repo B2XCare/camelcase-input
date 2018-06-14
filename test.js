ae22db90ab08ed245be2a0b1acc144fb98233256
CPatil <chetan.patil@b2x.com>
2018-06-14 12:50:07 +0530

    code changes according to older node js version

--- test.js
+++ test.js
@@ -4,7 +4,8 @@ const camelcase = require('./index.js').camelcase
 // console.log('222==', JSON.stringify(camelcase([{'FOo-bar': [{'abc-df__r': true}, {'tghd_dfdf--ee': true}]}, {'bar-foo': { 'Test-te': {'opt-tdt': 'dfdfdf'} }}], { deep: true })))
 // console.log('333==', camelcase({'foo-bar': true}))
 // console.log('444==', camelcase([{'foo-bar': true}, {'is_that_you': true}]))
-// console.log('STRING INPUT=> ', camelcase(['Foo-Bar', 'are-you-there']))
+console.log('STRING INPUT=> ', camelcase(['Foo-Bar', 'are-you-there']))
+// console.log('1111')
 a = { attributes:
 { type: 'Account',
   url: '/services/data/v40.0/sobjects/Account/0012500000qP7qcAAC' },
